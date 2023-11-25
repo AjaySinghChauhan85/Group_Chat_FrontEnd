85d4069471162eb7954de64de624320c2c10bd62
AjaySinghChauhan85 <chauhanajay505@gmail.com>
2023-11-25 22:48:59 +0530

    Add Enter Button Function and Chatting UI

--- src/Pages/DashBoard.js
+++ src/Pages/DashBoard.js
@@ -1,9 +1,24 @@
-import React from "react";
-
+import React, { useState } from "react";
 const DashBoard = () => {
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
   const handleLogout = () => {
     localStorage.removeItem("token");
-    window.location.reload()
+    window.location.reload();
+  };
+  const sendMessage = () => {
+    console.log("sendMessage function called");
+    if (newMessage.trim() === "") return;
+
+    const newMessages = [...messages, { text: newMessage, sender: "user" }];
+    setMessages(newMessages);
+    setNewMessage("");
+  };
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault(); // Prevents the default behavior (e.g., submitting a form)
+      sendMessage();
+    }
   };
   return (
     <div>
@@ -434,6 +449,114 @@ const DashBoard = () => {
             </div>
           </div>
         </div> */}
+
+        <div className="container mx-auto shadow-lg rounded-lg">
+          <div className="flex flex-row justify-between bg-white">
+            {/* <!-- chat list --> */}
+            <div className="flex flex-col w-2/5 border-r-2 overflow-y-auto">
+              {/* <!-- search compt --> */}
+              <div className="border-b-2 py-4 px-2">
+                <input
+                  type="text"
+                  placeholder="search chatting"
+                  className="py-2 px-2 border-2 border-gray-200 rounded-2xl w-full"
+                />
+              </div>
+              <div className="flex flex-row py-4 px-2 items-center border-b-2">
+                <div className="w-1/4">
+                  <img
+                    src="/Images/Ajay.jpg"
+                    className="object-cover h-12 w-12 rounded-full"
+                    alt=""
+                  />
+                </div>
+                <div className="w-full">
+                  <div className="text-lg font-semibold">Ajay</div>
+                  <span className="text-gray-500">
+                    Evan : some one can fix this
+                  </span>
+                </div>
+              </div>
+              {/* <!-- end user list --> */}
+            </div>
+            {/* <!-- end chat list -->
+                <!-- message --> */}
+            <div className="w-full px-5 flex flex-col justify-between">
+              <div className="flex flex-col mt-5">
+                <div className="flex justify-end mb-4">
+                  <div className="mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white">
+                    {messages.map((message, index) => (
+                      <div
+                        key={index}
+                        className="mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white"
+                      >
+                        {message.text}
+                      </div>
+                    ))}
+                  </div>
+                  <img
+                    src="/Images/Mountains.jpg"
+                    className="object-cover h-8 w-8 rounded-full"
+                    alt=""
+                  />
+                </div>
+                <div className="flex justify-start mb-4">
+                  <img
+                    src="/Images/Stars.jpg"
+                    className="object-cover h-8 w-8 rounded-full"
+                    alt=""
+                  />
+                  <div className="ml-2 py-3 px-4 bg-gray-400 rounded-br-3xl rounded-tr-3xl rounded-tl-xl text-white">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quaerat at praesentium, aut ullam delectus odio error sit
+                    rem. Architecto nulla doloribus laborum illo rem enim dolor
+                    odio saepe, consequatur quas?
+                  </div>
+                </div>
+                <div className="flex justify-end mb-4">
+                  <div>
+                    <div className="mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white">
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                      Magnam, repudiandae.
+                    </div>
+                  </div>
+                  <img
+                    src="/Images/Mountains.jpg"
+                    className="object-cover h-8 w-8 rounded-full"
+                    alt=""
+                  />
+                </div>
+                <div className="flex justify-start mb-4">
+                  <img
+                    src="/Images/Stars.jpg"
+                    className="object-cover h-8 w-8 rounded-full"
+                    alt=""
+                  />
+                  <div className="ml-2 py-3 px-4 bg-gray-400 rounded-br-3xl rounded-tr-3xl rounded-tl-xl text-white">
+                    happy holiday guys!
+                  </div>
+                </div>
+              </div>
+              <div className="py-5">
+                <input
+                  className="w-full bg-gray-300 py-5 px-3 rounded-xl"
+                  type="text"
+                  value={newMessage}
+                  onChange={(e) => setNewMessage(e.target.value)}
+                  onKeyDown={handleKeyPress}
+                  placeholder="type your message here..."
+                />
+                <button
+                  className="mt-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                  onClick={sendMessage}
+                >
+                  <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAACXBIWXMAAAsTAAALEwEAmpwYAAABMUlEQVR4nO3UzypFURTH8Y90BwZKKQM8gDKRl2BoYsyMmRE3KblznoIXkIxkZGJigiFFYSYKUVxHp/bN6Xb+3XvPRPnWGuy1d7/fOWvttfnnlyns4AaHKmIMqzhHlIjbXkQHMI8DfLYJt2K7U9F+zGAXrymC3/hIrONylWISW6GuUUa8Yz+YxOvLItFRrOAsR7QV91jCWyIX9ySVWRyhWUI4wimmcZfINUPjU3kqKRyFXgwFk2Q+/sBMNksIN1FHXzBp318oqn8jR/wFc+FcPWU/7sNgkUGWyXW4Ua1efaWc2SsjnmZygpGQn8Bzxh/Gc9IRi1hHLayHcZUh/hCGsWtqOM7pT/zI9cRGwe0q/TRksZwjfqEiGhkGa1UZpJnEgzeuYhpln4Ze2MRjN3f/7/EDz12xqRgiakMAAAAASUVORK5CYII=" />
+                </button>
+              </div>
+            </div>
+            {/* <!-- end message --> */}
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -32,6 +32,12 @@ const Login = () => {
         }
       });
   };
+  const handleEnter = (e) => {
+    if (e.key == "Enter") {
+      e.preventDefault();
+      handleLogin();
+    }
+  };
 
   return (
     <div>
@@ -90,6 +96,7 @@ const Login = () => {
                   placeholder="••••••••"
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   required=""
+                  onKeyPress={handleEnter}
                 />
               </div>
               <div className="flex items-center justify-between">
@@ -124,7 +131,6 @@ const Login = () => {
                 type="submit"
                 className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                 style={{ backgroundColor: "salmon" }}
-                onClick={handleLogin}
               >
                 Sign in
               </button>
